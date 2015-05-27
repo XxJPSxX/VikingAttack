19ec543d0dd1b226ca695aa65fdaae31df095c41
XxJPSxX <jp.medeiros_11@yahoo.com.br>
2015-05-26 23:20:17 -0300

    Atualização 1.2.3
    Melhoramentos no Código e início da programação da Torre

--- VickingAttack.py
+++ VickingAttack.py
@@ -6,6 +6,7 @@ from random import  randint
 from PPlay.collision import *
 from PPlay.animation import *
 from PPlay.sound import  *
+from PPlay.mouse import *
 from pygame import *
 #Subprogramas
 def inimigo(endereco, animacao, animacaoInicial, animacaoFinal, duracao, posX, posY):
@@ -60,17 +61,17 @@ def movimentacao(posX, posY, objeto, velocidadeObjeto):
         objeto.move_y(0)
         velX = -velocidadeObjeto
         velY = 0
-    if int(posX) in range(460, 465) and int(posY) in range(258, 90, -1):
+    if int(posX) in range(460, 465) and int(posY) in range(261, 90, -1):
         objeto.move_x(0)
         objeto.move_y(-velocidadeObjeto)
         velX = 0
         velY = -velocidadeObjeto
-    if int(posX) in range(460, 660) and int(posY) in range(90, 105):
+    if int(posX) in range(460, 661) and int(posY) in range(90, 105):
         objeto.move_x(velocidadeObjeto)
         objeto.move_y(0)
         velX = velocidadeObjeto
         velY = 0
-    if int(posX) in range(660, 670) and int(posY) in range(90, 100):
+    if int(posX) in range(661, 670) and int(posY) in range(90, 105):
         print("PERDEU")
     return velX, velY
 
@@ -96,7 +97,7 @@ def mudancaDeAnimacao(objeto, velX, velY, vetorSprites, verificadorObj):
         verificadorObj[1] = 0
     posX = objeto.x
     posY = objeto.y
-    if (verificadorObj[2] == 0 and posY in range(415, 500)) or (verificadorObj[2] ==0 and int(posX) in range(460, 660) and int(posY) in range(90, 100)):
+    if (verificadorObj[2] == 0 and posY in range(415, 500)) or (verificadorObj[2] ==0 and int(posX) in range(460, 661) and int(posY) in range(90, 101)):
         if velX > 0:
             posX = objeto.x
             posY = objeto.y
@@ -113,7 +114,7 @@ def mudancaDeAnimacao(objeto, velX, velY, vetorSprites, verificadorObj):
     if velX != 0:
         verificadorObj[3] = 0
     return objeto
-
+#Principal
 #Fundo
 background = GameImage("imagens\cenarios\cenario1.3.5.png")
 background.set_position(0, 0)
@@ -130,30 +131,49 @@ janela.set_background_color((0, 0, 0))
 icone = pygame.image.load("imagens\icone.jpg").convert_alpha()
 pygame.display.set_icon(icone)
 #Inimigos
+    #Soldado1
 soldado1, alturaSol1 = inimigo("imagens\soldado1\soldado1lado1.png", 9, 0, 9, 1000, 0, 140)
 vetorSoldado1 =["imagens\soldado1\soldado1lado1.png", "imagens\soldado1\soldado1lado2.png", "imagens\soldado1\soldado1frente.png", "imagens\soldado1\soldado1costas.png"] #Vetor para ser usado posteriormente
 vetorVerificadoresSol1 = [0, 0, 0, 0]
+velocidadeSol1 = 0.5
 #Musica
 musicaAtual = Sound("musicas\kingarthur.ogg")
 musicaAtual.set_volume(10)
+#Musica
+musicaAtual = Sound("musicas\kingarthur.ogg")
+musicaAtual.set_volume(10)
+#Waves (APENAS PARA TESTE)
+wave1 = 3000
+wave2 = 4000
+#Torres
 
+#Criei um quadrado com as coordenadas, que corresponde a área de ataque da torre do primeiro bloco de inserção
+def torre(posX, posY):
+    if (int(posX) in range(240, 420)) and (int(posY) in range(90, 320)):
+        print("DENTRO DA ÁREA DA TORRE")
+    return
 #GameLoop
 while True:
+
     janela.set_background_color((0, 0, 0))
     background.draw()
     #Inimigos
+
     #Soldado1
     soldado1.move_x(0)
     soldado1.move_y(0)
     soldado1.update()
     soldado1.draw()
-        #Movimentacao do Soldado1
+        #Movimentacao do Soldado 1
     posXSol1 = soldado1.x
     posYSol1 = soldado1.y
-    velX, velY = movimentacao(posXSol1, posYSol1, soldado1, 1)
-    print(velX, velY)
+    velXSol1, velYSol1 = movimentacao(posXSol1, posYSol1, soldado1, velocidadeSol1)
         #Troca de animações do Soldado 1
-    soldado1 = mudancaDeAnimacao(soldado1, velX, velY, vetorSoldado1, vetorVerificadoresSol1)
+    soldado1 = mudancaDeAnimacao(soldado1, velXSol1, velYSol1, vetorSoldado1, vetorVerificadoresSol1)
+    torre(posXSol1, posYSol1)
     #Musica
     musicaAtual.play()
+    #Mouse
+        #Muda o cursor OBS: https://www.pygame.org/docs/ref/cursors.html
+    pygame.mouse.set_cursor(*pygame.cursors.tri_left)
     janela.update()
