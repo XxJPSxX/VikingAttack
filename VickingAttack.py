e01f13b4edd5799307e1549a771c4b9bcae4c2be
XxJPSxX <jpsmedeiros@id.uff.br>
2015-05-29 01:48:59 -0300

    1.2.3.1
    Melhorias na implementação das torres

--- VickingAttack.py
+++ VickingAttack.py
@@ -14,13 +14,13 @@ def inimigo(endereco, animacao, animacaoInicial, animacaoFinal, duracao, posX, p
     #animacaoInicial = quadro inicial da animacao
     #animacaoFinal = quadro final da animacao
     #posX e posY = posições iniciais que o inimigo começa
-    soldado1 = Sprite(endereco, animacao)
-    larguraSol1 = soldado1.width
-    alturaSol1 = soldado1.height
-    soldado1.set_sequence(animacaoInicial, animacaoFinal) # Tem que ser até apenas o 5 pois o Sprite está com problema.
-    soldado1.set_total_duration(duracao)
-    soldado1.set_position(posX, posY-alturaSol1)
-    return soldado1, alturaSol1
+    soldado = Sprite(endereco, animacao)
+    larguraSol = soldado.width
+    alturaSol = soldado.height
+    soldado.set_sequence(animacaoInicial, animacaoFinal) # Tem que ser até apenas o 5 pois o Sprite está com problema.
+    soldado.set_total_duration(duracao)
+    soldado.set_position(posX, posY-alturaSol)
+    return soldado, alturaSol
 def movimentacao(posX, posY, objeto, velocidadeObjeto):
     #Condições diferentes para cada cenário, tais coordenadas valem para o cenário 1.3.5
     #Troca de animação dependendo da direção do movimento(A fazer...)
@@ -114,6 +114,29 @@ def mudancaDeAnimacao(objeto, velX, velY, vetorSprites, verificadorObj):
     if velX != 0:
         verificadorObj[3] = 0
     return objeto
+
+def torre(imagemTorre, bloco):
+    torre = GameImage(imagemTorre)
+    larguraTorre = (torre.width/2)
+    alturaTorre = (torre.height/2)+20 #Sem o +20 a imagem fica exatamente no meio, com o +20 ela fica melhor posicionada
+    #bloco[0] = coordenada X do centro do bloco, bloco[1]= coordenada Y do centro do bloco
+    torre.set_position(bloco[0]-larguraTorre, bloco[1]-alturaTorre)
+    return torre
+
+def torreAtira(listaTorre, bloco, listaAlvo):
+    #Criei um quadrado com as coordenadas, que corresponde a área de ataque da torre
+    #listaTorre = [sprite, alcanceX, alcanceY, taxaDeDisparo, dano]
+    #listaSoldado = [soldado(sprite), alturaSoldado, vidaSoldado]
+    #Alcance em X
+    areaX1 = bloco[0] - listaTorre[1]
+    areaX2 = bloco[0] + listaTorre[1]
+    #Alcance em Y
+    areaY1 = (bloco[1] - listaTorre[2])-listaAlvo[1]
+    areaY2 = bloco[1] + listaTorre[2]
+    if listaAlvo[0].x in range(areaX1, areaX2) and listaAlvo[0].y in range(areaY1, areaY2):
+        print("DENTRO DA AREA DA TORRE")
+    #    listaAlvo[2] = listaAlvo[2] - listaTorre[4]
+    return
 #Principal
 #Fundo
 background = GameImage("imagens\cenarios\cenario1.3.5.png")
@@ -132,26 +155,31 @@ icone = pygame.image.load("imagens\icone.jpg").convert_alpha()
 pygame.display.set_icon(icone)
 #Inimigos
     #Soldado1
+#inimigo(endereco, animacao, animacaoInicial, animacaoFinal, duracao, posX, posY)
 soldado1, alturaSol1 = inimigo("imagens\soldado1\soldado1lado1.png", 9, 0, 9, 1000, 0, 140)
-vetorSoldado1 =["imagens\soldado1\soldado1lado1.png", "imagens\soldado1\soldado1lado2.png", "imagens\soldado1\soldado1frente.png", "imagens\soldado1\soldado1costas.png"] #Vetor para ser usado posteriormente
+vidaSol1 = 100
+#listaSoldado = [soldado(sprite), alturaSoldado, vidaSoldado]
+listaSoldado1 = [soldado1, alturaSol1, vidaSol1]
+vetorSoldado1 =["imagens\soldado1\soldado1lado1.png", "imagens\soldado1\soldado1lado2.png", "imagens\soldado1\soldado1frente.png", "imagens\soldado1\soldado1costas.png"]
 vetorVerificadoresSol1 = [0, 0, 0, 0]
-velocidadeSol1 = 0.5
+velocidadeSol1 = 1
 #Musica
 musicaAtual = Sound("musicas\kingarthur.ogg")
 musicaAtual.set_volume(10)
-#Musica
-musicaAtual = Sound("musicas\kingarthur.ogg")
-musicaAtual.set_volume(10)
+musicaAtual.play()
 #Waves (APENAS PARA TESTE)
 wave1 = 3000
 wave2 = 4000
 #Torres
+    #Centros dos Blocos de Inserção(aproximado)
+bloco1 = [322, 212]
+bloco2 = [181, 388]
+bloco3 = [444, 378]
+spriteTorre1 = "imagens\Torres\TorrePedra.png" #Tive que definir o sprite antes
+torre1 = torre(spriteTorre1, bloco1)
+#listaTorre = [objeto, alcanceX, alcanceY, taxaDeDisparo, dano]
+listaTorre1 = [torre1, 100, 100, "??", 10] #VALORES DESTINADOS À TESTE
 
-#Criei um quadrado com as coordenadas, que corresponde a área de ataque da torre do primeiro bloco de inserção
-def torre(posX, posY):
-    if (int(posX) in range(240, 420)) and (int(posY) in range(90, 320)):
-        print("DENTRO DA ÁREA DA TORRE")
-    return
 #GameLoop
 while True:
 
@@ -160,19 +188,23 @@ while True:
     #Inimigos
 
     #Soldado1
-    soldado1.move_x(0)
-    soldado1.move_y(0)
-    soldado1.update()
-    soldado1.draw()
+    listaSoldado1[0].move_x(0)
+    listaSoldado1[0].move_y(0)
+    listaSoldado1[0].update()
+    listaSoldado1[0].draw()
         #Movimentacao do Soldado 1
-    posXSol1 = soldado1.x
-    posYSol1 = soldado1.y
-    velXSol1, velYSol1 = movimentacao(posXSol1, posYSol1, soldado1, velocidadeSol1)
+    posXSol1 = listaSoldado1[0].x
+    posYSol1 = listaSoldado1[0].y
+    velXSol1, velYSol1 = movimentacao(posXSol1, posYSol1, listaSoldado1[0], velocidadeSol1)
         #Troca de animações do Soldado 1
-    soldado1 = mudancaDeAnimacao(soldado1, velXSol1, velYSol1, vetorSoldado1, vetorVerificadoresSol1)
-    torre(posXSol1, posYSol1)
+    listaSoldado1[0] = mudancaDeAnimacao(listaSoldado1[0], velXSol1, velYSol1, vetorSoldado1, vetorVerificadoresSol1)
+    #torre(posXSol1, posYSol1)
+    #Torres
+    listaTorre1[0].draw()
+    torreAtira(listaTorre1, bloco1, listaSoldado1)
     #Musica
-    musicaAtual.play()
+    #musicaAtual.play() Temporariamente coloquei o .play() fora do loop
+    #PROBLEMA!! Música sendo reproduzida mais de uma vez ao mesmo tempo
     #Mouse
         #Muda o cursor OBS: https://www.pygame.org/docs/ref/cursors.html
     pygame.mouse.set_cursor(*pygame.cursors.tri_left)
